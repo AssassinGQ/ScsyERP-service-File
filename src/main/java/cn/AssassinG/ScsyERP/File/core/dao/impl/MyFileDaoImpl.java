3ffdbf723b1e7b5c5a9b13c5187b14843b17d82d
AssassinG <hangq_zju@hotmail.com>
2018-12-14 15:34:17 +0800

    init commit

---
+++ src/main/java/cn/AssassinG/ScsyERP/File/core/dao/impl/MyFileDaoImpl.java
@@ -0,0 +1,27 @@
+package cn.AssassinG.ScsyERP.File.core.dao.impl;
+
+import cn.AssassinG.ScsyERP.File.core.dao.MyFileDao;
+import cn.AssassinG.ScsyERP.File.facade.entity.MyFile;
+import cn.AssassinG.ScsyERP.common.core.dao.BaseDaoImpl;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository(value="MyFileDao")
+public class MyFileDaoImpl extends BaseDaoImpl<MyFile> implements MyFileDao {
+
+    @Override
+    public void update(MyFile entity) {}
+
+    @Override
+    public int update(List<MyFile> list) {return 0;}
+//
+//    @Autowired
+//    public void setSqlSession(SqlSessionTemplate sqlSessionTemplate){
+//        super.setSqlSessionTemplate(sqlSessionTemplate);
+//    }
+//    @Autowired
+//    public void setSessionTemplate(SqlSessionTemplate sqlSessionTemplate){
+//        super.sessionTemplate = sqlSessionTemplate;
+//    }
+}
